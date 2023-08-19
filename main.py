8759bdda79575e6643fccb941fba9114c71c2558
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-08-19 19:47:03 +0200

    a

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import streamlit_elements as elements
 
 st.title("Math")
 
-st.session_state["boxes"] = []
+st.session_state["boxes"] = 0
 if st.button("Add a new box"):
-    with st.session_state["boxes"].append(elements.elements(len(st.session_state["boxes"]))):
+    with elements.elements(st.session_state["boxes"]):
         tabs = st.tabs(["View", "Edit"])
+    st.session_state["boxes"] += 1
