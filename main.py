f2347d977626304f4350b9937e5bd682afc43d48
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-08-19 22:08:19 +0200

    a

--- main.py
+++ main.py
@@ -3,8 +3,26 @@ import streamlit_elements as elements
 
 st.title("Math")
 
-st.session_state["boxes"] = 0
+st.session_state.layout = []
+st.session_state.text = []
+
+page = elements.elements("page")
+
 if st.button("Add a new box"):
-    with elements.elements(st.session_state["boxes"]):
+    with page:
+        # Add a new tab
+        st.session_state.layout.append(elements.dashboard.Item(i=f"Box {len(st.session_state.layout)+1}", x=len(st.session_state.layout)%2, y=len(st.session_state.layout)//2, w=1, h=1))
+        with elements.dashboard.Grid([st.session_state.layout[-1]]):
+            tabs = st.tabs(["View", "Edit"])
+            with tabs[0]:
+                def update_text(value):
+                    st.session_state.text[-1] = value
+                elements.editor.Monaco(
+                    height=200,
+                    value="",
+                    onChange=update_text,
+                )
+                elements.mui.Button("Update content", onClick=elements.sync())
+            with tabs[1]:
+                st.write(st.session_state.text[-1])
         tabs = st.tabs(["View", "Edit"])
-    st.session_state["boxes"] += 1
