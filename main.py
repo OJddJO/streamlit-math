8456dda735b309adf15c67ea5487a475f516fa44
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-08-19 22:28:16 +0200

    a

--- main.py
+++ main.py
@@ -3,17 +3,14 @@ import streamlit as st
 st.set_page_config(page_title="Math", page_icon="🟰", layout="wide")
 st.title("Math")
 
-st.session_state.text = 'Hello World!'
+st.session_state.text = ''
 
 columns = st.columns(2)
 
-def update_data():
-    st.session_state.text = input
-    st.experimental_rerun()
-
 with columns[0]:
-    input = st.text_area(label="Input", placeholder="Input", key="input", value=st.session_state.text, height=200, label_visibility="collapsed")
+    input = st.text_area(label="Input", placeholder="Input", key="input", height=200, label_visibility="collapsed")
     if st.button("Update"):
-        update_data()
+        st.session_state.text = input
+        st.experimental_rerun()
 with columns[1]:
     st.latex(st.session_state.text)
