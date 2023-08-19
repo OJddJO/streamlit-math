b198d705ca9e42a5f9e5d2a555e15602bf0a3a85
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-08-19 22:24:14 +0200

    a

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ st.session_state.text = 'Hello World!'
 
 columns = st.columns(2)
 
-with columns[0]:
-    input = st.text_area(label="Input", placeholder="Input", key="input", value=st.session_state.text, height=200, label_visibility="collapsed")
-    if st.button("Update"):
-        st.session_state.text = input
-        st.experimental_rerun()
+def update_data():
+    st.session_state.text = input
+    st.experimental_rerun()
 
+with columns[0]:
+    input = st.text_area(label="Input", placeholder="Input", key="input", value=st.session_state.text, height=200, label_visibility="collapsed", on_change=update_data)
+    if st.button("Update"):
+        update_data()
 with columns[1]:
     st.latex(st.session_state.text)
