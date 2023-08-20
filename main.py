fd3e13dde51a05765f5fd8f18f761c5f06d15454
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-08-20 11:06:49 +0200

    a

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ st.session_state.text = ''
 
 columns = st.columns(2)
 
-def update_text(input):
+def update_text():
     st.session_state.text = input
     with columns[1]:
         st.latex(st.session_state.text)
 
 with columns[0]:
-    input = st.text_area(label="Input", placeholder="Input", key="input", height=200, label_visibility="collapsed", on_change=lambda: update_text(input))
+    input = st.text_area(label="Input", placeholder="Input", key="input", height=200, label_visibility="collapsed", on_change=update_text)
     if st.button("Update"):
-        update_text(input)
+        update_text()
