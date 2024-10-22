de16ad2f36ddafa82f53b276b472c27c02d3c756
Jeremy Li <jeremyli2906@hotmail.com>
2024-10-22 15:36:27 +0200

    Update error

--- Math.py
+++ Math.py
@@ -157,56 +157,59 @@ st.session_state.text = []
 st.session_state.latex_container = []
 
 def evaluate_latex(text):
-    latex = ""
-    latex_dict = json.load(open("latex.json", "r"))
-    latex_func = json.load(open("latex_func.json", "r"))
-    i = 0
-    while text != '':
-        #check if text[:i] is in latex_dict
-        if text[:i] in latex_func: #if it is
-            func = latex_func[text[:i]] #add latex to func
-            latex += func[0]
-            nb_args = func[1] #get number of args
-            text = text[i:]
-            #check nested in func
-            i = 0
-            nested = 0
-            end = False
-            arg_list = []
-            while nb_args > 0:
-                while not end:
-                    if text[i] == '(':
-                        nested += 1
-                    elif text[i] == ')':
-                        nested -= 1
-                    if nested == 0:
-                        end = True
-                        if func[0] == "\\text{¤1}":
-                            arg_list.append(text[1:i])
-                        else:
-                            arg_list.append(evaluate_latex(text[1:i]))
-                    i += 1
-                end = False
+    try:
+        latex = ""
+        latex_dict = json.load(open("latex.json", "r"))
+        latex_func = json.load(open("latex_func.json", "r"))
+        i = 0
+        while text != '':
+            #check if text[:i] is in latex_dict
+            if text[:i] in latex_func: #if it is
+                func = latex_func[text[:i]] #add latex to func
+                latex += func[0]
+                nb_args = func[1] #get number of args
                 text = text[i:]
+                #check nested in func
                 i = 0
-                nb_args -= 1
-            for i, arg in enumerate(arg_list):
-                latex = latex.replace(f"¤{i+1}", arg)
-            i = 0 
-            text = text[i:] #remove from text
-        elif text[:i] in latex_dict: #if in latex_dict
-            latex += latex_dict[text[:i]] #add latex
-            text = text[i:] #remove from text
-            i = 0
-        else: #if not
-            i += 1
-            if i > len(text):
-                if text.find("\n") == 0:
-                    latex += '\\\\ '
-                else:
-                    latex += text[0]
-                text = text[1:]
+                nested = 0
+                end = False
+                arg_list = []
+                while nb_args > 0:
+                    while not end:
+                        if text[i] == '(':
+                            nested += 1
+                        elif text[i] == ')':
+                            nested -= 1
+                        if nested == 0:
+                            end = True
+                            if func[0] == "\\text{¤1}":
+                                arg_list.append(text[1:i])
+                            else:
+                                arg_list.append(evaluate_latex(text[1:i]))
+                        i += 1
+                    end = False
+                    text = text[i:]
+                    i = 0
+                    nb_args -= 1
+                for i, arg in enumerate(arg_list):
+                    latex = latex.replace(f"¤{i+1}", arg)
+                i = 0 
+                text = text[i:] #remove from text
+            elif text[:i] in latex_dict: #if in latex_dict
+                latex += latex_dict[text[:i]] #add latex
+                text = text[i:] #remove from text
                 i = 0
+            else: #if not
+                i += 1
+                if i > len(text):
+                    if text.find("\n") == 0:
+                        latex += '\\\\ '
+                    else:
+                        latex += text[0]
+                    text = text[1:]
+                    i = 0
+    except:
+        latex = text + ' (Error)'
     return latex
 
 def update_text(page, text):
