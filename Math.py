a09c2436cdb2a1b359f6efbeb2a89dc836c7ffc0
Jeremy Li <jeremyli2906@hotmail.com>
2024-10-22 16:39:24 +0200

    a

--- Math.py
+++ Math.py
@@ -212,14 +212,18 @@ def evaluate_latex(text):
         latex += '\\\\\\color{red}\\text{!! Error !!}'
     return latex
 
-def update_text():
-    st.session_state.text = input
-    latex = evaluate_latex(input)
+def get_text():
+    text = input
+    update_text(text)
+
+def update_text(text):
+    st.session_state.text = text
+    latex = evaluate_latex(text)
     latex_container.latex(latex)
 
 latex_container = st.container()
 with st.expander(label="Input Zone", expanded=True):
-    input = st.text_area(label="Input", placeholder="Input", value=st.session_state.text, key="input", height=400, label_visibility="collapsed", on_change=update_text)
+    input = st.text_area(label="Input", placeholder="Input", value=st.session_state.text, key="input", height=400, label_visibility="collapsed", on_change=get_text)
     col1, col2 = st.columns(2)
     if col1.button("Submit"):
         update_text(input)
