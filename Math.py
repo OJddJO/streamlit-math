c5c94b81ec913062a627d18e49523933474c18bb
Jeremy Li <jeremyli2906@hotmail.com>
2024-10-22 15:39:40 +0200

    error

--- Math.py
+++ Math.py
@@ -209,7 +209,7 @@ def evaluate_latex(text):
                     text = text[1:]
                     i = 0
     except:
-        latex += '\\text{ (Error)}'
+        latex += '\\text{\n!!Error!!}'
     return latex
 
 def update_text(page, text):
