e3153a62cc15a876795f54b7846e7a2b0b19ef76
Jeremy Li <jeremyli2906@hotmail.com>
2024-10-22 16:31:36 +0200

    a

--- Math.py
+++ Math.py
@@ -225,4 +225,5 @@ with st.form(key="input_form"):
         update_text(input)
     if col2.form_submit_button("Clear"):
         st.session_state.text = ""
-        input = st.text_area(label="Input", placeholder="Input", value=st.session_state.text, key="input", height=400, label_visibility="collapsed")
+        input.value = ""
+        st.rerun()
