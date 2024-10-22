9e726dbfc478c730d7b560dccd9e34ef6a7a0c8f
Jeremy Li <jeremyli2906@hotmail.com>
2024-10-22 15:43:50 +0200

    error

--- Math.py
+++ Math.py
@@ -209,7 +209,7 @@ def evaluate_latex(text):
                     text = text[1:]
                     i = 0
     except:
-        latex += '\\\\\\htmlStyle{color: red;}{!! Error !!}'
+        latex += '\\\\\\textcolor{red}{!! Error !!}'
     return latex
 
 def update_text(page, text):
